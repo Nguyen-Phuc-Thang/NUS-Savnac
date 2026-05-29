ba620678a3d876a8fe90bd61b4c0f583d52ab92d
Nguyen-Phuc-Thang <nguyen.phuc.thang.vn@gmail.com>
2026-05-30 00:13:02 +0700

    Login and register page done (not tested)

---
+++ frontend/lib/auth.ts
@@ -0,0 +1,43 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions = {
+    providers: [
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: { label: "email", type: "text" },
+                password: { label: "password", type: "password" },
+            },
+            async authorize(credentials) {
+                const res = await fetch(
+                    `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
+                    {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json",
+                        },
+                        body: JSON.stringify({
+                            email: credentials?.email,
+                            password: credentials?.password,
+                        }),
+                    }
+                );
+
+                const user = await res.json();
+
+                if (!res.ok) return null;
+
+                return user;
+            },
+        }),
+    ],
+
+    session: {
+        strategy: "jwt" as const,
+    },
+
+    secret: process.env.AUTH_SECRET,
+};
+
+export default NextAuth(authOptions);
