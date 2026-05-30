90a3c0f4b708211c00d25703d8977759d593545c
hhkennn <hhken1129@gmail.com>
2026-05-30 23:33:07 +0800

    feat(frontend): scaffold task-list feature

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,17 +1,25 @@
 import type { Metadata } from "next";
 import "./globals.css";
-
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/app-sidebar";
 
 export const metadata: Metadata = {
   title: "NUS Savnac",
   description: "A website for Orbital 26",
 };
 
-export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode; }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body className="min-h-full flex flex-col">
-        {children}
+        <SidebarProvider>
+          <AppSidebar />
+          <main className="flex-1">
+            {children}
+          </main>
+        </SidebarProvider>
       </body>
     </html>
   );
