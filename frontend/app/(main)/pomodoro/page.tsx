3db48bc50a32bd315ba1c184a770eb4021a995b2
Nguyen-Phuc-Thang <nguyen.phuc.thang.vn@gmail.com>
2026-06-28 10:03:52 +0700

    Pomodoro Integrated

--- frontend/app/(main)/pomodoro/page.tsx
+++ frontend/app/(main)/pomodoro/page.tsx
@@ -2,7 +2,7 @@ import Timer from "@/components/timer";
 
 const Pomodoro = () => {
   return (
-    <div className="min-h-screen flex flex-col items-center justify-center p-4">
+    <div className="flex flex-col width-full min-h-screen gap-4 ml-6 mt-6 items-center">
       <h1 className="text-3xl font-bold mb-8 text-gray-800">Pomodoro Timer</h1>
       <Timer />
     </div>
