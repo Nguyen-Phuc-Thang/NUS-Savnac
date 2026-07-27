9647984bf41beb0dd59111398c8c8e361585fe24
Nguyen-Phuc-Thang <nguyen.phuc.thang.vn@gmail.com>
2026-07-27 23:56:41 +0700

    Course Components Test

--- frontend/components/course/CourseCard.tsx
+++ frontend/components/course/CourseCard.tsx
@@ -1,33 +1,38 @@
-import {
-    CircularProgressbar,
-    buildStyles,
-} from "react-circular-progressbar";
+import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 
-import "react-circular-progressbar/dist/styles.css";
-import { taskCompletionProgress } from "@/lib/utils/task";
+import 'react-circular-progressbar/dist/styles.css';
+import { taskCompletionProgress } from '@/lib/utils/task';
 
 interface CourseCardProps {
     courseCode: string;
     title: string;
-    tasks: any[],
+    tasks: any[];
     onClick?: () => void;
 }
 
-export default function CourseCard({ courseCode, title, tasks, onClick }: CourseCardProps) {
+export default function CourseCard({
+    courseCode,
+    title,
+    tasks,
+    onClick,
+}: CourseCardProps) {
     const { ratio, text } = taskCompletionProgress(tasks);
 
     return (
-        <button className={`w-60 border rounded-md p-4 flex flex-col justify-between shadow-sm transition-all duration-200 ease-out hover:-translate-y-1 hover:shadow-lg hover:border-secondary hover:ring hover:ring-secondary`} onClick={onClick}>
+        <button
+            className={`w-60 border rounded-md p-4 flex flex-col justify-between shadow-sm transition-all duration-200 ease-out hover:-translate-y-1 hover:shadow-lg hover:border-secondary hover:ring hover:ring-secondary`}
+            onClick={onClick}
+        >
             <div className="flex items-center justify-center">
                 <div className="w-40 h-40">
                     <CircularProgressbar
                         value={ratio * 100}
                         text={text}
                         styles={buildStyles({
-                            pathColor: "#003062",
-                            trailColor: "#e5e7eb",
-                            textColor: "#111827",
-                            textSize: "16px",
+                            pathColor: '#003062',
+                            trailColor: '#e5e7eb',
+                            textColor: '#111827',
+                            textSize: '16px',
                         })}
                     />
                 </div>
@@ -36,7 +41,9 @@ export default function CourseCard({ courseCode, title, tasks, onClick }: Course
                 <p className="font-sans font-medium text-md">{courseCode}</p>
             </div>
             <div className="h-15 mt-2">
-                <p className="font-sans text-sm text-muted-foreground">{title}</p>
+                <p className="font-sans text-sm text-muted-foreground">
+                    {title}
+                </p>
             </div>
         </button>
     );
---
+++ frontend/components/course/DeleteCourseDialog.test.tsx
@@ -0,0 +1,90 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { describe, expect, it, vi } from 'vitest';
+
+import DeleteCourseDialog from './DeleteCourseDialog';
+
+const mockCourse = {
+    courseCode: 'CS1010S',
+    courseTitle: 'Programming Methodology',
+    tasks: [],
+};
+
+describe('Delete Course Dialog', () => {
+    it('does not display the dialog when open is false', () => {
+        render(
+            <DeleteCourseDialog
+                open={false}
+                onOpenChange={vi.fn()}
+                course={mockCourse}
+                onDeleteCourse={vi.fn()}
+            />,
+        );
+
+        expect(screen.queryByRole('dialog')).not.toBeInTheDocument();
+
+        expect(screen.queryByText('Delete Course')).not.toBeInTheDocument();
+    });
+    it('displays the dialog and all courses when open is true', () => {
+        render(
+            <DeleteCourseDialog
+                open={true}
+                onOpenChange={vi.fn()}
+                course={mockCourse}
+                onDeleteCourse={vi.fn()}
+            />,
+        );
+        expect(screen.queryByRole('dialog')).toBeInTheDocument();
+
+        expect(screen.queryByText('Delete Course')).toBeInTheDocument();
+    });
+    it('contains course title in the dialog when open is true', () => {
+        render(
+            <DeleteCourseDialog
+                open={true}
+                onOpenChange={vi.fn()}
+                course={mockCourse}
+                onDeleteCourse={vi.fn()}
+            />,
+        );
+        expect(
+            screen.queryByText(
+                'Are you sure you want to delete Programming Methodology? This action cannot be undone.',
+            ),
+        ).toBeInTheDocument();
+    });
+    it('calls onDeleteCourse when the delete button is clicked', async () => {
+        const onDeleteCourseMock = vi.fn();
+        const user = userEvent.setup();
+        render(
+            <DeleteCourseDialog
+                open={true}
+                onOpenChange={vi.fn()}
+                course={mockCourse}
+                onDeleteCourse={onDeleteCourseMock}
+            />,
+        );
+
+        const deleteButton = screen.getByRole('button', { name: 'Delete' });
+        await user.click(deleteButton);
+
+        expect(onDeleteCourseMock).toHaveBeenCalledOnce();
+    });
+    it('calls onOpenChange with false when the dialog is closed', async () => {
+        const onOpenChangeMock = vi.fn();
+        const user = userEvent.setup();
+        render(
+            <DeleteCourseDialog
+                open={true}
+                onOpenChange={onOpenChangeMock}
+                course={mockCourse}
+                onDeleteCourse={vi.fn()}
+            />,
+        );
+
+        const closeButton = screen.getByRole('button', { name: /close/i });
+        await user.click(closeButton);
+
+        expect(onOpenChangeMock).toHaveBeenCalledWith(false);
+    });
+});
