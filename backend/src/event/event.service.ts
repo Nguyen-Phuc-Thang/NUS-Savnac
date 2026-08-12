93f2dc52fc4bd3887f4541e46e98744a2d919ef5
Nguyen-Phuc-Thang <nguyen.phuc.thang.vn@gmail.com>
2026-08-12 15:27:08 +0800

    Bug fixed

--- backend/src/event/event.service.ts
+++ backend/src/event/event.service.ts
@@ -1,68 +1,97 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
-
 
 @Injectable()
 export class EventService {
-    constructor(private prisma: PrismaService) { }
+  constructor(private prisma: PrismaService) {}
 
-    async getEventsByUserId(userId: string) {
-        return this.prisma.client.event.findMany({
-            where: {
-                userId: userId,
-            },
-            include: {
-                course: true,
-            }
-        });
-    }
+  async getEventsByUserId(userId: string) {
+    return this.prisma.client.event.findMany({
+      where: {
+        userId: userId,
+      },
+      include: {
+        course: true,
+      },
+    });
+  }
 
-    async getEventsByCourseId(courseId: string) {
-        return this.prisma.client.event.findMany({
-            where: {
-                courseId: courseId,
-            },
-        });
-    }
+  async getEventsByCourseId(courseId: string) {
+    return this.prisma.client.event.findMany({
+      where: {
+        courseId: courseId,
+      },
+    });
+  }
 
-    async addEvent(userId: string, eventType: "CLASS" | "DEADLINE" | "EXAM" | "OTHERS", eventTitle: string, eventWeek: string, eventDay: string, eventStartTime: string, eventEndTime: string, eventVenue?: string, courseId?: string) {
-        return this.prisma.client.event.create({
-            data: {
-                userId: userId,
-                eventType: eventType,
-                courseId: courseId,
-                title: eventTitle,
-                week: eventWeek,
-                day: eventDay,
-                startTime: eventStartTime,
-                endTime: eventEndTime,
-                venue: eventVenue,
-            }
-        });
+  async addEvent(
+    userId: string,
+    eventType: 'CLASS' | 'DEADLINE' | 'EXAM' | 'OTHERS',
+    eventTitle: string,
+    eventWeek: string,
+    eventDay: string,
+    eventStartTime: string,
+    eventEndTime: string,
+    eventVenue?: string,
+    courseId?: string,
+  ) {
+    try {
+      return this.prisma.client.event.create({
+        data: {
+          userId: userId,
+          eventType: eventType,
+          courseId: courseId,
+          title: eventTitle,
+          week: eventWeek,
+          day: eventDay,
+          startTime: eventStartTime,
+          endTime: eventEndTime,
+          venue: eventVenue,
+        },
+      });
+    } catch (error: any) {
+      if (
+        error instanceof Prisma.PrismaClientKnownRequestError &&
+        error.code === 'P2002'
+      ) {
+        throw new BadRequestException(`${eventTitle} have already been added!`);
+      }
+      throw error;
     }
+  }
 
-    async updateEvent(eventId: string, eventType: "CLASS" | "DEADLINE" | "EXAM" | "OTHERS", eventTitle: string, eventWeek: string, eventDay: string, eventStartTime: string, eventEndTime: string, eventVenue?: string) {
-        return this.prisma.client.event.update({
-            where: {
-                eventId: eventId,
-            },
-            data: {
-                eventType: eventType,
-                title: eventTitle,
-                week: eventWeek,
-                day: eventDay,
-                startTime: eventStartTime,
-                endTime: eventEndTime,
-                venue: eventVenue,
-            }
-        });
-    }
+  async updateEvent(
+    eventId: string,
+    eventType: 'CLASS' | 'DEADLINE' | 'EXAM' | 'OTHERS',
+    eventTitle: string,
+    eventWeek: string,
+    eventDay: string,
+    eventStartTime: string,
+    eventEndTime: string,
+    eventVenue?: string,
+  ) {
+    return this.prisma.client.event.update({
+      where: {
+        eventId: eventId,
+      },
+      data: {
+        eventType: eventType,
+        title: eventTitle,
+        week: eventWeek,
+        day: eventDay,
+        startTime: eventStartTime,
+        endTime: eventEndTime,
+        venue: eventVenue,
+      },
+    });
+  }
 
-    async deleteEvent(eventId: string) {
-        return this.prisma.client.event.delete({
-            where: {
-                eventId: eventId,
-            }
-        });
-    }
+  async deleteEvent(eventId: string) {
+    return this.prisma.client.event.delete({
+      where: {
+        eventId: eventId,
+      },
+    });
+  }
 }
--- frontend/components/event/ScheduleCalendar.tsx
+++ frontend/components/event/ScheduleCalendar.tsx
@@ -1,19 +1,31 @@
-"use client";
+'use client';
 
 // React Hooks
-import { useEffect, useState } from "react";
+import { useEffect, useState } from 'react';
 
 // UI Components
-import FullCalendar from "@fullcalendar/react";
-import { Button } from "@/components/ui/button";
-import { weeks } from "@/lib/constants/time";
-import timeGridPlugin from "@fullcalendar/timegrid";
-import { ChevronRight, ChevronLeft, Plus } from "lucide-react";
-import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
-import { Combobox, ComboboxContent, ComboboxEmpty, ComboboxInput, ComboboxItem, ComboboxList } from "@/components/ui/combobox";
+import FullCalendar from '@fullcalendar/react';
+import { Button } from '@/components/ui/button';
+import { weeks } from '@/lib/constants/time';
+import timeGridPlugin from '@fullcalendar/timegrid';
+import { ChevronRight, ChevronLeft, Plus } from 'lucide-react';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {
+    Combobox,
+    ComboboxContent,
+    ComboboxEmpty,
+    ComboboxInput,
+    ComboboxItem,
+    ComboboxList,
+} from '@/components/ui/combobox';
 
 // Utils
-import { modifyEvent } from "@/lib/utils/event";
+import { modifyEvent } from '@/lib/utils/event';
 
 interface ScheduleCalendarProps {
     events: any[];
@@ -21,27 +33,35 @@ interface ScheduleCalendarProps {
     onAddClassClick?: () => void;
     onAddEventClick?: () => void;
 }
-export default function ScheduleCalendar({ events, onEventClick, onAddClassClick, onAddEventClick }: ScheduleCalendarProps) {
-
-    const [currentWeek, setCurrentWeek] = useState<string>("Week 1");
+export default function ScheduleCalendar({
+    events,
+    onEventClick,
+    onAddClassClick,
+    onAddEventClick,
+}: ScheduleCalendarProps) {
+    const [currentWeek, setCurrentWeek] = useState<string>('Week 1');
     const [eventsToDisplay, setEventsToDisplay] = useState<any[]>([]);
 
     const handleWeekChange = (newWeek: string) => {
         setCurrentWeek(newWeek);
-        setEventsToDisplay(events.filter(event => event.week === newWeek).map(event => modifyEvent(event)));
-    }
+        setEventsToDisplay(
+            events
+                .filter((event) => event.week === newWeek)
+                .map((event) => modifyEvent(event)),
+        );
+    };
 
     const handlePrevWeek = async () => {
         const currentWeekIndex = weeks.indexOf(currentWeek);
         if (currentWeekIndex == 0) return;
         handleWeekChange(weeks[currentWeekIndex - 1]);
-    }
+    };
 
     const handleNextWeek = async () => {
         const currentWeekIndex = weeks.indexOf(currentWeek);
         if (currentWeekIndex == weeks.length - 1) return;
         handleWeekChange(weeks[currentWeekIndex + 1]);
-    }
+    };
 
     useEffect(() => {
         handleWeekChange(currentWeek);
@@ -49,32 +69,56 @@ export default function ScheduleCalendar({ events, onEventClick, onAddClassClick
 
     return (
         <div>
-            <div className='mt-5 font-sans flex flex-row items-center'>
-                <Combobox items={weeks} value={currentWeek} onValueChange={(value) => handleWeekChange(value ?? "")}>
+            <div className="mt-5 font-sans flex flex-row items-center">
+                <Combobox
+                    items={weeks}
+                    value={currentWeek}
+                    onValueChange={(value) => handleWeekChange(value ?? '')}
+                >
                     <ComboboxInput placeholder="Select a week" />
                     <ComboboxContent>
                         <ComboboxEmpty>No items found.</ComboboxEmpty>
                         <ComboboxList className="max-h-none">
                             {(item) => (
-                                <ComboboxItem className='font-sans' key={item} value={item}>
+                                <ComboboxItem
+                                    className="font-sans"
+                                    key={item}
+                                    value={item}
+                                >
                                     {item}
                                 </ComboboxItem>
                             )}
                         </ComboboxList>
                     </ComboboxContent>
                 </Combobox>
-                <Button className='ml-5 bg-white text-black border shadow-sm transition-colors hover:bg-white' onClick={handlePrevWeek}><ChevronLeft /></Button>
-                <Button className='ml-2 bg-white text-black border shadow-sm transition-colors hover:bg-white' onClick={handleNextWeek}><ChevronRight /></Button>
-                <div className='font-sans ml-5'>
+                <Button
+                    className="ml-5 bg-white text-black border shadow-sm transition-colors hover:bg-white"
+                    onClick={handlePrevWeek}
+                >
+                    <ChevronLeft />
+                </Button>
+                <Button
+                    className="ml-2 bg-white text-black border shadow-sm transition-colors hover:bg-white"
+                    onClick={handleNextWeek}
+                >
+                    <ChevronRight />
+                </Button>
+                <div className="font-sans ml-5">
                     <DropdownMenu>
                         <DropdownMenuTrigger className="flex flex-row items-center justify-center font-sans px-2 py-2 rounded-md border hover:bg-primary hover:text-white shadow-sm transition-colors">
                             <Plus className="w-4 h-4" />
                         </DropdownMenuTrigger>
                         <DropdownMenuContent>
-                            <DropdownMenuItem className="font-sans mt-1" onClick={onAddClassClick}>
+                            <DropdownMenuItem
+                                className="font-sans mt-1"
+                                onClick={onAddClassClick}
+                            >
                                 NUS Class
                             </DropdownMenuItem>
-                            <DropdownMenuItem className="font-sans mt-2 mb-1" onClick={onAddEventClick}>
+                            <DropdownMenuItem
+                                className="font-sans mt-2 mb-1"
+                                onClick={onAddEventClick}
+                            >
                                 Custom Event
                             </DropdownMenuItem>
                         </DropdownMenuContent>
@@ -82,7 +126,11 @@ export default function ScheduleCalendar({ events, onEventClick, onAddClassClick
                 </div>
             </div>
 
-            <div className='mt-10'>
+            <div className="mt-5 text-sm font-sans italic text-gray-500">
+                Click on an event to view its details
+            </div>
+
+            <div className="mt-5">
                 <FullCalendar
                     plugins={[timeGridPlugin]}
                     initialView="timeGridWeek"
@@ -92,28 +140,21 @@ export default function ScheduleCalendar({ events, onEventClick, onAddClassClick
                     slotMaxTime="23:00:00"
                     headerToolbar={false}
                     dayHeaderFormat={{
-                        weekday: "short"
+                        weekday: 'short',
                     }}
                     events={eventsToDisplay}
                     eventClick={onEventClick}
-                    eventContent={
-                        (info) => (
-                            <div className="overflow-hidden text-xs">
-                                <div className="font-semibold">
-                                    {info.event.title}
-                                </div>
-                                <div>
-                                    {info.event.extendedProps.course}
-                                </div>
-                                <div>
-                                    {info.event.extendedProps.venue}
-                                </div>
+                    eventContent={(info) => (
+                        <div className="overflow-hidden text-xs">
+                            <div className="font-semibold">
+                                {info.event.title}
                             </div>
-                        )
-                    }
+                            <div>{info.event.extendedProps.course}</div>
+                            <div>{info.event.extendedProps.venue}</div>
+                        </div>
+                    )}
                 />
             </div>
         </div>
     );
-
 }
